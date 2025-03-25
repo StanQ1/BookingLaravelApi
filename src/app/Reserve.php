2bfd69667d4a63fdd0e47901ed94e461c4c64e5c
Max Stanishevsky <xa1per11@gmail.com>
2025-03-25 08:24:20 +0100

    BelongsTo functions added to Reserve.php

--- src/app/Reserve.php
+++ src/app/Reserve.php
@@ -3,12 +3,23 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Reserve extends Model
 {
     protected $fillable = [
-        'room_id',
+        'room_number',
         'date_from',
         'date_to',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function room(): BelongsTo
+    {
+        return $this->belongsTo(Room::class);
+    }
 }

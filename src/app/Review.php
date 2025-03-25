0889b89481535892126cad14220af54b8e9007ae
Max Stanishevsky <xa1per11@gmail.com>
2025-03-25 08:36:16 +0100

    BelongsTo HasMany functions added to Review.php

--- src/app/Review.php
+++ src/app/Review.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Review extends Model
 {
@@ -10,4 +11,14 @@ class Review extends Model
         'comment',
         'rating',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function hotel(): BelongsTo
+    {
+        return $this->belongsTo(Hotel::class);
+    }
 }

8ab3394ecae36ad0acb5ac3a9948dd862fee1ef9
Max Stanishevsky <xa1per11@gmail.com>
2025-03-25 08:36:04 +0100

    BelongsTo HasMany functions added to Payment.php

--- src/app/Payment.php
+++ src/app/Payment.php
@@ -3,10 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Payment extends Model
 {
     protected $fillable = [
         'payment_method'
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function reservation(): BelongsTo
+    {
+        return $this->belongsTo(Reserve::class);
+    }
 }

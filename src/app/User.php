846bb4c59039eecc5293c5668dcfd916c234da8c
Max Stanishevsky <xa1per11@gmail.com>
2025-03-25 08:30:27 +0100

    BelongsTo HasMany functions added to User.php

--- src/app/User.php
+++ src/app/User.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Model
 {
@@ -11,4 +12,24 @@ class User extends Model
         'email',
         'password',
     ];
+
+    public function reservations(): HasMany
+    {
+        return $this->hasMany(Reserve::class);
+    }
+
+    public function hotels(): HasMany
+    {
+        return $this->hasMany(Hotel::class);
+    }
+
+    public function payments(): HasMany
+    {
+        return $this->hasMany(Payment::class);
+    }
+
+    public function reviews(): HasMany
+    {
+        return $this->hasMany(Review::class);
+    }
 }

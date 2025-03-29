05d703925eecf854fb075f2df76d215463085f86
Max Stanishevsky <xa1per11@gmail.com>
2025-03-29 12:21:32 +0100

    Room API was added

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HotelController;
+use App\Http\Controllers\RoomController;
 use App\Http\Middleware\Owner;
 use Illuminate\Support\Facades\Route;
 
@@ -11,8 +12,15 @@ Route::prefix('auth')->group(function () {
 });
 
 Route::prefix('hotel')->group(function () {
-    Route::get('/all', [HotelController::class, 'index']);
+    Route::get('/', [HotelController::class, 'index']);
     Route::get('/{id}', [HotelController::class, 'show']);
+    Route::get('/{hotelId}/room', [HotelController::class, 'rooms']);
+    Route::get('/{hotelId}/room/{roomNumber}', [HotelController::class, 'room']);
+});
+
+Route::prefix('room')->group(function () {
+    Route::get('/', [RoomController::class, 'index']);
+    Route::get('/{id}', [RoomController::class, 'show']);
 });
 
 Route::group([
@@ -22,5 +30,11 @@ Route::group([
         Route::post('/create', [HotelController::class, 'create']);
         Route::post('/update', [HotelController::class, 'update']);
         Route::post('/delete', [HotelController::class, 'delete']);
+    });
+
+    Route::prefix('room')->group(function () {
+        Route::post('/create', [RoomController::class, 'create']);
+        Route::post('/update', [RoomController::class, 'update']);
+        Route::post('/delete', [RoomController::class, 'delete']);
     });
 });
